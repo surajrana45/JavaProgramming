ff7221340e8f869d639a5b57e8b7e67733217416
user <user@DESKTOP-LFGF7K9>
2022-03-24 19:38:50 +0530

    first commit

--- javaBasicProgram/src/Unit_01/P1_Task02_FristJavaBaseCode.java
+++ javaBasicProgram/src/Unit_01/P1_Task02_FristJavaBaseCode.java
@@ -1,5 +1,29 @@
 package Unit_01;
 
-public class P1_Task02_FristJavaBaseCode {
+import Unit_01.P1_Task01_FirstJavaBaseCode.Main;
 
+public class P1_Task01_FirstJavaBaseCode {
+
+	public Static Void main(String[] args) {
+		//Syntax error on token "float", invalid VariableDeclaratorId
+		int float=23;
+		//multiple markers at this line
+		//- Main cannot be resolved to a 
+		 //variable
+		Main int=new Main();
+		//Syntax error on token "int", invalid Expression
+		System.out.println(int.a);
+		}
+		//Multiple markers at this line
+		//- Syntax error, insert ";" to complete 
+		// MethodDeclaration
+		//- Syntax error on token "int", Identifier expected
+		//Syntax error on token "int", Identifier expected
+		void int() {
+			int a=10;
+		}
+		public class Main{
+			int a=1;
+		   
+		}
 }
--- javaBasicProgram/src/Unit_01/P2_Task02_ConstaintsJava.java
+++ javaBasicProgram/src/Unit_01/P2_Task02_ConstaintsJava.java
@@ -2,4 +2,21 @@ package Unit_01;
 
 public class P2_Task02_ConstaintsJava {
 
+	public static final int a=10;
+	final double pi=3.14
+	public static void main(String[] args) {
+		int b=1;
+		System.out.println(a);
+		//Cannot make a static reference to the non-static field pi
+		System.out.println(pi);
+		//Cannot make a static reference to the non-static method main() from the type 
+		//P2_Task02_ConstantsinJava
+		main();
+	}
+
+	public void main() {
+		System.out.println(a);
+		//b cannot be resolved to a variable
+		System.out.println(b);
+	}
 }
